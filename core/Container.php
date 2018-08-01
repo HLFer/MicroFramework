e6aa9d47e71e4f5697f9ef34350bd7571566628e
HLFer <henry1991.contato@gmail.com>
2018-07-31 22:10:23 -0300

    Criando o primeiro Controller

---
+++ core/Container.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+    public static function newController($controller)
+    {
+        $controller = 'App\\Controllers\\'.$controller;
+
+        return  new $controller();
+    }
+}
--- core/Route.php
+++ core/Route.php
@@ -2,53 +2,55 @@
 
 namespace Core;
 
-class Route{
+class Route
+{
+    private $routes;
 
-  private $routes;
-
-  public function __construct(array $routes){
-    $this->setRoutes($routes);
-    $this->run();
-  }
-
-  private function setRoutes($routes){
-    foreach ($routes as $route) {
-      $explode = explode('@', $route[1]);
-      $r = [$route[0], $explode[0], $explode[1]];
-      $newRoutes[] = $r;
+    public function __construct(array $routes)
+    {
+        $this->setRoutes($routes);
+        $this->run();
     }
-    $this->routes = $newRoutes;
-  }
 
-  private function getUrl(){
-    return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-  }
-
-  private function run(){
-
-    $url = $this->getUrl();
-    $urlArray = explode('/', $url);
-
-    foreach ($this->routes as $route) {
-      $routeArray = explode('/', $route[0]);
-      for($i = 0; $i < count($routeArray); $i++){
-        if((strpos($routeArray[$i], '{') !== false) && (count($urlArray) == count($routeArray))){
-          $routeArray[$i] = $urlArray[$i];
-          $param[] = $urlArray[$i];
+    private function setRoutes($routes)
+    {
+        foreach ($routes as $route) {
+            $explode = explode('@', $route[1]);
+            $r = [$route[0], $explode[0], $explode[1]];
+            $newRoutes[] = $r;
         }
-        $route[0] = implode($routeArray, '/');
-      }
-      if($url == $route[0]){
-        $found = true;
-        $controller = $route[1];
-        $action = $route[2];
-        break;
-      }
+        $this->routes = $newRoutes;
     }
 
-    if($found){
-      
+    private function getUrl()
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
-  }
+    private function run()
+    {
+        $url = $this->getUrl();
+        $urlArray = explode('/', $url);
+
+        foreach ($this->routes as $route) {
+            $routeArray = explode('/', $route[0]);
+            for ($i = 0; $i < count($routeArray); ++$i) {
+                if ((strpos($routeArray[$i], '{') !== false) && (count($urlArray) == count($routeArray))) {
+                    $routeArray[$i] = $urlArray[$i];
+                    $param[] = $urlArray[$i];
+                }
+                $route[0] = implode($routeArray, '/');
+            }
+            if ($url == $route[0]) {
+                $found = true;
+                $controller = $route[1];
+                $action = $route[2];
+                break;
+            }
+        }
+
+        if ($found) {
+            \Core\Container::newController($controller);
+        }
+    }
 }
